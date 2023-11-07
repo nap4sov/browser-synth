2e26a625e957224c361cc1876b5c9b8249670b20
Roman <g4vr0m@gmail.com>
2023-11-07 14:59:32 +0200

    update keyboard listeners, add note stop delay

--- src/audio/modules/ADSR.ts
+++ src/audio/modules/ADSR.ts
@@ -11,7 +11,7 @@ export default class ADSR extends Gain {
 
   private decayTime = 0.1;
 
-  isPlaying = false;
+  private envelopeActive = false;
 
   constructor(synthCtx: AudioContext, parentGainNode: Gain) {
     super(synthCtx);
@@ -24,34 +24,47 @@ export default class ADSR extends Gain {
   };
 
   play = () => {
+    this.envelopeActive = true;
     // reset gain
-    this.gainNode.gain.setValueAtTime(0, this.synthCtx.currentTime);
+    this.gainNode.gain.setValueAtTime(
+      this.gainNode.gain.value,
+      this.synthCtx.currentTime,
+    );
 
     // ramp to peak
     this.gainNode.gain.linearRampToValueAtTime(
       this.sustainLevel * (1 + this.peakMultiplier),
       this.synthCtx.currentTime + this.attackTime,
     );
-
     // ramp to sustain level
     this.gainNode.gain.linearRampToValueAtTime(
       this.sustainLevel,
       this.synthCtx.currentTime + this.attackTime + this.decayTime,
     );
+
+    setTimeout(() => {
+      this.envelopeActive = false;
+    }, (this.attackTime + this.decayTime) * 1000);
   };
 
   stop = () => {
-    // reset gain
-    this.gainNode.gain.setValueAtTime(
-      this.sustainLevel,
-      this.synthCtx.currentTime,
-    );
+    // suppress stop function from being called before attack and decay took place
 
-    // release
-    this.gainNode.gain.linearRampToValueAtTime(
-      0,
-      this.synthCtx.currentTime + this.releaseTime,
-    );
+    if (this.envelopeActive) {
+      setTimeout(this.stop, (this.attackTime + this.decayTime) * 1000);
+    } else {
+      // reset gain
+      this.gainNode.gain.setValueAtTime(
+        this.sustainLevel,
+        this.synthCtx.currentTime,
+      );
+
+      // release
+      this.gainNode.gain.linearRampToValueAtTime(
+        0,
+        this.synthCtx.currentTime + this.releaseTime,
+      );
+    }
   };
 
   setAttackTime = (value: number) => {
--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -6,12 +6,10 @@ const KEY_WIDTH = 60;
 
 const Keyboard = ({ keyboard }: { keyboard: SynthesizerKeyboard }) => {
   const [currentNote, setCurrentNote] = useState('');
-  const [mousePressed, setMousePressed] = useState(false);
+  const [pointerPressed, setPointerPressed] = useState(false);
 
   const handleNoteChange = (x: number, y: number, isStart: boolean) => {
     const element = document.elementFromPoint(x, y);
-    if (element?.nodeName !== 'BUTTON') return;
-
     const note = element?.getAttribute('data-note');
 
     if (!note || note === currentNote) return;
@@ -27,69 +25,64 @@ const Keyboard = ({ keyboard }: { keyboard: SynthesizerKeyboard }) => {
   const handleNoteStop = () => {
     if (!currentNote) return;
 
-    setCurrentNote('');
     keyboard[currentNote].stop();
+    setCurrentNote('');
   };
 
   useEffect(() => {
-    const handleMousePressed = () => {
-      setMousePressed(true);
+    const handlePointerPressed = () => {
+      setPointerPressed(true);
     };
-    const handleMouseReleased = () => {
-      setMousePressed(false);
+    const handlePointerReleased = () => {
+      setPointerPressed(false);
     };
-    window.addEventListener('mousedown', handleMousePressed);
-    window.addEventListener('mouseup', handleMouseReleased);
+    window.addEventListener('pointerdown', handlePointerPressed);
+    window.addEventListener('pointerup', handlePointerReleased);
 
     return () => {
-      window.removeEventListener('mousedown', handleMousePressed);
-      window.removeEventListener('mouseup', handleMouseReleased);
+      window.removeEventListener('pointerdown', handlePointerPressed);
+      window.removeEventListener('pointerup', handlePointerReleased);
     };
   }, []);
 
   return (
-    <div
-      className="keyboard"
-      onTouchStart={(e) => {
-        handleNoteChange(e.touches[0].pageX, e.touches[0].pageY, true);
-      }}
-      onTouchMove={(e) => {
-        handleNoteChange(e.touches[0].pageX, e.touches[0].pageY, false);
-      }}
-      onTouchEnd={handleNoteStop}
-      onMouseDown={(e) => {
-        handleNoteChange(e.clientX, e.clientY, true);
-      }}
-      onMouseEnter={(e) => {
-        if (!mousePressed) return;
-        handleNoteChange(e.clientX, e.clientY, false);
-      }}
-      onMouseMove={(e) => {
-        if (!mousePressed) return;
-        handleNoteChange(e.clientX, e.clientY, false);
-      }}
-      onMouseUp={handleNoteStop}
-      onMouseLeave={handleNoteStop}
-    >
-      {Object.keys(keyboard).map((note, idx) => {
-        const isBlack =
-          idx === 1 || idx === 3 || idx === 6 || idx === 8 || idx === 10;
-        return (
-          <button
-            type="button"
-            key={note}
-            data-note={note}
-            className={isBlack ? 'note black' : 'note white'}
-            style={{
-              left:
-                (KEY_WIDTH / 2) * idx +
-                KEY_WIDTH / 4 +
-                (idx >= 6 ? KEY_WIDTH / 2 : 0),
-              width: isBlack ? KEY_WIDTH / 2 : KEY_WIDTH,
-            }}
-          />
-        );
-      })}
+    <div className="keyboard-wrapper">
+      <div
+        className="keyboard"
+        onPointerDown={(e) => {
+          handleNoteChange(e.clientX, e.clientY, true);
+        }}
+        onPointerEnter={(e) => {
+          if (!pointerPressed) return;
+          handleNoteChange(e.clientX, e.clientY, false);
+        }}
+        onPointerMove={(e) => {
+          if (!pointerPressed) return;
+          handleNoteChange(e.clientX, e.clientY, false);
+        }}
+        onPointerUp={handleNoteStop}
+        onPointerLeave={handleNoteStop}
+        onPointerCancel={handleNoteStop}
+      >
+        {Object.keys(keyboard).map((note, idx) => {
+          const isBlack =
+            idx === 1 || idx === 3 || idx === 6 || idx === 8 || idx === 10;
+          return (
+            <div
+              key={note}
+              data-note={note}
+              className={isBlack ? 'note black' : 'note white'}
+              style={{
+                left:
+                  (KEY_WIDTH / 2) * idx +
+                  KEY_WIDTH / 4 +
+                  (idx >= 6 ? KEY_WIDTH / 2 : 0),
+                width: isBlack ? KEY_WIDTH / 2 : KEY_WIDTH,
+              }}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
